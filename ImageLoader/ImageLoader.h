f5660e37fc1c2c3e3b651c641649b9022b973516
Andrew Mayers <andrew@mobelux.com>
2016-11-02 19:27:09 -0400

    initial project creation

---
+++ ImageLoader/ImageLoader.h
@@ -0,0 +1,19 @@
+//
+//  ImageLoader.h
+//  ImageLoader
+//
+//  Created by Jerry Mayers on 11/2/16.
+//  Copyright © 2016 Mobelux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageLoader.
+FOUNDATION_EXPORT double ImageLoaderVersionNumber;
+
+//! Project version string for ImageLoader.
+FOUNDATION_EXPORT const unsigned char ImageLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageLoader/PublicHeader.h>
+
+
