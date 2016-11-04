cad7fd8ff9d8a20fe942c149177e8c150725bb16
Andrew Mayers <andrew@mobelux.com>
2016-11-04 10:27:27 -0400

    added UIImageView image loading using ImageLoader

---
+++ ImageLoader/Extensions/UIImageView+Loading.swift
@@ -0,0 +1,60 @@
+//
+//  UIImageView+Loading.swift
+//  ImageLoader
+//
+//  Created by Jerry Mayers on 11/4/16.
+//  Copyright © 2016 Mobelux. All rights reserved.
+//
+
+import UIKit
+
+private let loadingImageViews: NSMapTable = NSMapTable<UIImageView, TaskBox>(keyOptions: .weakMemory, valueOptions: .strongMemory)
+
+private class TaskBox {
+    var task: ImageLoader.LoadingTask
+    init(task: ImageLoader.LoadingTask) {
+        self.task = task
+    }
+}
+
+public extension UIImageView {
+    private enum Constants {
+        static let imageFadeDuration: TimeInterval = 0.3
+    }
+
+    /// Loads an image from a server using `ImageLoader`. When the image is loaded it will be shown in this view. If the image load completes and came from the cache, we will just show the image immediately. This is because cache loads should be near instant. But if the image is downloaded, then we will fade the image in, gradually replacing this view's current `image`.
+    ///
+    /// - Parameters:
+    ///   - imageURL: The URL to the image to load
+    ///   - loader: The image loader instance to use. If none is given then the shared loader will be used
+    ///   - completion: Called once the load & fade animation is finished. `success` will be `true` if we were able to load the requested image.
+    public func load(imageURL: URL, loader: ImageLoader = ImageLoader.shared, completion: ((_ success: Bool) -> Void)? = nil) {
+        cancelImageLoad()
+
+        let task = loader.image(from: imageURL) { (image, fromCache) in
+            guard let image = image else {
+                completion?(false)
+                return
+            }
+
+            if fromCache {
+                self.image = image
+                completion?(true)
+            } else {
+                self.fadeTo(image: image, duration: Constants.imageFadeDuration, completion: {
+                    completion?(true)
+                })
+            }
+        }
+
+        let taskBox = TaskBox(task: task)
+        loadingImageViews.setObject(taskBox, forKey: self)
+    }
+
+    /// Cancels any image loading for this image view. If the image view is in a table/collection view cell/header you should call this in `prepareForReuse()`. It is called automatically when you start loading a new image on this view using the `load(imageURL:completion:)` method, so no need to call it just prior to calling that.
+    public func cancelImageLoad() {
+        guard let taskBox = loadingImageViews.object(forKey: self) else { return }
+        taskBox.task.cancel()
+        loadingImageViews.removeObject(forKey: self)
+    }
+}
--- ImageLoaderTests/ImageViewTests.swift
+++ ImageLoaderTests/ImageViewTests.swift
@@ -11,6 +11,27 @@ import XCTest
 
 class ImageViewTests: XCTestCase {
 
+    private var cache: URLCache?
+
+    override func setUp() {
+        super.setUp()
+        cache?.removeAllCachedResponses()
+    }
+
+    override func tearDown() {
+        cache?.removeAllCachedResponses()
+        super.tearDown()
+    }
+
+    private func configureLoader() -> (ImageLoader, Session, MockReachability) {
+        let reachable = MockReachability()
+        let cache = URLCache(memoryCapacity: 100 * 1024 * 1024, diskCapacity: 0, diskPath: nil)
+        self.cache = cache
+        let session = MockSession(reachable: reachable, cache: cache)
+        let loader = ImageLoader(session: session, cache: cache, reachable: reachable)
+        return (loader, session, reachable)
+    }
+
     private func getImages() -> (mobelux: UIImage, blank: UIImage) {
         let blankImage = UIImage()
         let mobeluxURL = Bundle(for: ImageViewTests.self).url(forResource: "mobelux", withExtension: "png")!
@@ -37,4 +58,58 @@ class ImageViewTests: XCTestCase {
         waitForExpectations(timeout: 4, handler: nil)
     }
 
+    func testImageLoading() {
+        let (loader, _, _) = configureLoader()
+
+        let imageURL = URL(string: "http://mobelux.com/static/img/mobelux-mark.99537226e971.png")!
+        let imageExpection = expectation(description: "Image loading")
+
+        let view = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.load(imageURL: imageURL, loader: loader) { (success) in
+            XCTAssert(success, "Didn't load the image")
+            XCTAssertNotNil(view.image, "Didn't load the image")
+
+            imageExpection.fulfill()
+        }
+
+        waitForExpectations(timeout: 4, handler: nil)
+    }
+
+    func testImageLoadingNoNetwork() {
+        let (loader, _, reachable) = configureLoader()
+
+        reachable.isReachable = false
+
+        let imageURL = URL(string: "http://mobelux.com/static/img/mobelux-mark.99537226e971.png")!
+        let imageExpection = expectation(description: "Image loading")
+
+        let view = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.load(imageURL: imageURL, loader: loader) { (success) in
+            XCTAssertFalse(success, "We shouldn't be able to load the image when we can't reach the network")
+            XCTAssertNil(view.image, "We shouldn't be able to load the image when we can't reach the network")
+
+            imageExpection.fulfill()
+        }
+
+        waitForExpectations(timeout: 4, handler: nil)
+    }
+
+    func testImageLoadingCancelation() {
+        let (loader, _, _) = configureLoader()
+
+        let imageURL = URL(string: "http://mobelux.com/static/img/mobelux-mark.99537226e971.png")!
+        let imageExpection = expectation(description: "Image loading")
+
+        let view = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.load(imageURL: imageURL, loader: loader) { (success) in
+            XCTAssert(false, "We should never get here")
+        }
+        view.cancelImageLoad()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            imageExpection.fulfill()
+        }
+
+        waitForExpectations(timeout: 4, handler: nil)
+    }
 }
